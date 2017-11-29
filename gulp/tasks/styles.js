5b3e7c28f30a9b4e3bd4f8284cfa9a41f57d2771
Chazona Baum <chznbaum@gmail.com>
2017-11-29 16:11:19 -0500

    completed header block and intro content block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp'),
+notify = require('gulp-notify'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
+
+const onError = function (errorMsg) {
+  notify({
+    title: 'Gulp Task Error',
+    message: 'Check the console.'
+  }).write(errorMsg);
+
+  console.log(errorMsg.toString());
+
+  this.emit('end');
+}
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/css/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', onError)
+    .pipe(gulp.dest('./app/temp/css'));
+});
