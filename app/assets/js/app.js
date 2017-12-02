627bc63ce4e4d91669bcb7478a80bad02fb138e9
Chazona Baum <chznbaum@gmail.com>
2017-12-02 12:55:13 -0500

    completed reveal on scroll feature

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
+const featureItemReveal = new RevealOnScroll($('.feature-item'), '85%');
+const testimonialReveal = new RevealOnScroll($('.testimonial'), '60%');
