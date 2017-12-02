4b89f0b0c612aed57dd50e767f01052579745437
Chazona Baum <chznbaum@gmail.com>
2017-12-02 16:00:21 -0500

    completed modal feature

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyNav from './modules/StickyNav';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
 const featureItemReveal = new RevealOnScroll($('.feature-item'), '85%');
 const testimonialReveal = new RevealOnScroll($('.testimonial'), '60%');
 const stickyNav = new StickyNav();
+const modal = new Modal();
